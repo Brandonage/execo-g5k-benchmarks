c9932e20aed86102af4b45457c4295d80c5ff04c
Brandonage <alvarobrandon@gmail.com>
2017-06-06 15:38:57 +0200

    changed ycsb for cassandra to do SshProcess

--- ycsb/cassandraycsb.py
+++ ycsb/cassandraycsb.py
@@ -1,20 +1,20 @@
 from ycsb import YCSB
-from execo import Remote
+from execo import Remote, SshProcess
 import sys
 
-class CassandraYCSB(YCSB):
 
-    def __init__(self,install_nodes,execo_conn_params,cassandra_nodes):
+class CassandraYCSB(YCSB):
+    def __init__(self, install_nodes, execo_conn_params, cassandra_nodes):
         """
 
         :param install_nodes: a set containing the nodes where ycsb will be installed 
         :param execo_conn_params: a dictionary including the connection parameters for execo
         :param cassandra_nodes: a set containing the nodes that constitute the cassandra cluster
         """
-        YCSB.__init__(self,install_nodes,execo_conn_params)
+        YCSB.__init__(self, install_nodes, execo_conn_params)
         self.cassandra_nodes = cassandra_nodes
 
-    def load_workload(self,from_node, workload, recordcount, insertcount, insertstart,threadcount=1):
+    def load_workload(self, from_node, workload, recordcount, threadcount=1):
         """
         Run a workload from the core workloads with the CassandraDB db wrapper
         :param from_node: from which node you want to run the workload. This is a set variable
@@ -27,19 +27,34 @@ class CassandraYCSB(YCSB):
         """
         # we transform the set to a str with the format 'node1,node2,node3...'
         cassandra_nodes_str = ','.join(list(self.cassandra_nodes))
+        # divide the number of records equally among clients
+        insertcount = int(recordcount) / from_node.__len__()
+        insertstart_steps = list(range(from_node.__len__()))
+        insertstart_values = map(lambda x: x * insertcount, insertstart_steps)
         # We load the data into the cassandra database
-        Remote("ycsb-0.12.0/bin/ycsb.sh load cassandra-cql -P ycsb-0.12.0/workloads/{0} -p hosts={1} -p recordcount={2} -p insertstart={3} -p insertcount={4} -p threadcount={5}"
-               .format(workload,cassandra_nodes_str,recordcount,insertstart,insertcount,threadcount),
-               hosts=from_node,
-               connection_params=self.execo_conn_params,
-               process_args={'stdout_handlers': [sys.stdout], 'stderr_handlers': [sys.stderr]}).run()
+        # This could be change to an execo generator expression like insertstart={{[x for x in insertstart_values]}}
+        for start_value in insertstart_values:
+            SshProcess(
+                "ycsb-0.12.0/bin/ycsb.sh load cassandra-cql -P ycsb-0.12.0/workloads/{0} -p hosts={1} -p recordcount={2} -p insertstart={3} -p insertcount={4} -p threadcount={5}"
+                .format(workload, cassandra_nodes_str, recordcount, start_value, insertcount, threadcount),
+                host=from_node,
+                connection_params=self.execo_conn_params,
+                stdout_handlers=[sys.stdout],
+                stderr_handlers = [sys.stderr]).run()
 
-    def run_workload(self,from_node, workload, threadcount=1):
+    def run_workload(self, iteration, res_dir, from_node, workload, threadcount=1):
         # we transform the set to a str with the format 'node1,node2,node3...'
+        """
+        Run a given workload. it receives an iteration parameter to be able to repeat the workload several times
+        :param iteration: the iteration number. Will be appended to the name of the file with the output
+        :param from_node: from which nodes should we execute the benchmark
+        :param workload: the type of workload   
+        :param threadcount: the number of threads
+        """
         cassandra_nodes_str = ','.join(list(self.cassandra_nodes))
         # We run the workload
-        Remote("ycsb-0.12.0/bin/ycsb.sh run cassandra-cql -P ycsb-0.12.0/workloads/{0} -p hosts={1} -p threadcount={2}"
-               .format(workload,cassandra_nodes_str,threadcount),
-               hosts=from_node,
-               connection_params=self.execo_conn_params,
-               process_args={'stdout_handlers': [sys.stdout], 'stderr_handlers': [sys.stderr]}).run()
+        Remote(
+            "ycsb-0.12.0/bin/ycsb.sh run cassandra-cql -P ycsb-0.12.0/workloads/" + workload + " -p hosts=" + cassandra_nodes_str + " -p threadcount=" + str(threadcount) + " -p exportfile=" + res_dir + "/output_" + workload + "_{{{host}}}_it" + str(iteration),
+            hosts=from_node,
+            connection_params=self.execo_conn_params,
+            process_args={'stdout_handlers': [sys.stdout], 'stderr_handlers': [sys.stderr]}).run()
